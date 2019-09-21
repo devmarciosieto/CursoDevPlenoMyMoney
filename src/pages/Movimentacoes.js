68438b5dfc1a91438b6973badf7dddaae632f10d
Márcio Rogério Sieto <mmmsieto@gmail.com>
2019-09-21 13:37:11 -0300

    first commit

--- src/pages/Movimentacoes.js
+++ src/pages/Movimentacoes.js
@@ -7,9 +7,9 @@ const { useGet, usePost, useDelete, usePatch } = Rest(baseURL)
 const Movimentacoes = ({match}) => {
     const data = useGet(`movimentacoes/${match.params.data}`)
     const dataMeses = useGet(`meses/${match.params.data}`)
-    const [, patch] = usePatch()
-    const [, salvar] = usePost(`movimentacoes/${match.params.data}`)
-    const [, remover]= useDelete()
+    const [dataPatch, patch] = usePatch()
+    const [postData, salvar] = usePost(`movimentacoes/${match.params.data}`)
+    const [removeData, remover]= useDelete()
     const [descricao, setDescricao] = useState('')
     const [valor, setValor] = useState('')
 
